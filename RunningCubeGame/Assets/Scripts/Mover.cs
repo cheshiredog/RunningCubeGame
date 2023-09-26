163b2e95266b8ea7a5f023f6efab824e8a4e32a0
cheshiredog <cheshiredog1865@gmail.com>
2023-09-26 19:44:59 +0300

    Walls and rigidbody added

--- RunningCubeGame/Assets/Scripts/Mover.cs
+++ RunningCubeGame/Assets/Scripts/Mover.cs
@@ -9,11 +9,23 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PringInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PringInstructions()
+    {
+        Debug.Log("Welcome to the game.\n" +
+                  "Press W A S D keys to move cube. " +
+                  "Don't bump into the obstacles.");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
